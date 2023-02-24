80422e9784e6c393da5c747c89aa608d210eac3f
Normilando <86866114+Normilando@users.noreply.github.com>
2023-02-24 19:13:09 -0300

    O botão = agora limpa a tela.

---
+++ calcular.js
@@ -0,0 +1,61 @@
+let tela = document.getElementById("tela");
+
+function one() {
+    return tela.textContent += 1;
+}
+
+function two() {
+    return tela.textContent += 2;
+}
+
+function three() {
+    return tela.textContent += 3;
+}
+
+function four() {
+    return tela.textContent += 4;
+}
+
+function five() {
+    return tela.textContent += 5;
+}
+
+function six() {
+    return tela.textContent += 6;
+}
+
+function seven() {
+    return tela.textContent += 7;
+}
+
+function eight() {
+    return tela.textContent += 8;
+}
+
+function nine() {
+    return tela.textContent += 9;
+}
+
+function zero() {
+    return tela.textContent += 0;
+}
+
+function plus() {
+    return tela.textContent += "+";
+}
+
+function minus() {
+    return tela.textContent += "-";
+}
+
+function division() {
+    return tela.textContent += "/";
+}
+
+function multiply() {
+    return tela.textContent += "*";
+}
+
+function equal() {
+    
+}
